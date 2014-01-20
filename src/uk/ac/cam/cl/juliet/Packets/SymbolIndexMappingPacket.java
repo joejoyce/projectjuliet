7a7615ea60e20264c3f85836f3c9567349c7fb6b
slwilliams <slw69@cam.ac.uk>
2014-01-20 20:45:24 +0000

    Added Symbol Index Mapping packet

---
+++ src/uk/ac/cam/cl/juliet/Packets/SymbolIndexMappingPacket.java
@@ -0,0 +1,44 @@
+package uk.ac.cam.cl.juliet.Packets;
+
+import java.io.RandomAccessFile;
+import java.io.IOException;
+
+public class SymbolIndexMappingPacket extends Packet
+{
+	private long symbolIndex;
+	private String symbol;
+	private long marketID;
+	private long systemID;
+	private char exchangeCode;
+	private long priceScaleCode;
+	private char securityType;
+	private long lotSize;
+	private long prevClosePrice;
+	private long prevCLoseVolume;
+	private long priceResolution;
+	private char roundLot;
+	
+	public SymbolIndexMappingPacket(RandomAccessFile file) throws IOException
+	{
+		super(file);		
+					
+		this.symbolIndex = readLong(4);	
+		this.symbol = readString(11);		
+			
+		//Jump the filler
+		file.read();		
+			
+		this.marketID = readLong(2);
+		this.systemID = readLong(1);
+		
+		this.exchangeCode = file.readChar();		
+		this.priceScaleCode = readLong(1);
+		this.securityType = file.readChar();
+		this.lotSize = readLong(2);
+		
+		this.prevClosePrice = readLong(4);
+		this.prevCLoseVolume = readLong(4);
+		this.priceResolution = readLong(4);
+		this.roundLot = file.readChar();
+	}
+}
