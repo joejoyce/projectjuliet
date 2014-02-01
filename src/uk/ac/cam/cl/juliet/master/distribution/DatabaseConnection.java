0e2536fef1b848c426038a899fd342760bef366b
Lucas Sonnabend <ls594@cam.ac.uk>
2014-02-01 22:04:54 +0000

    Added new interface DatabaseConnection to encapsulate the db access from the XDP Processor (Idea behind it: use mock implementation to test decoding and count number/type of messages etc.) Also added decoding for source time reference message in XDP Processor

---
+++ src/uk/ac/cam/cl/juliet/master/distribution/DatabaseConnection.java
@@ -0,0 +1,85 @@
+package uk.ac.cam.cl.juliet.master.distribution;
+
+import java.sql.SQLException;
+/**
+ * This encapsulates the database accesses made by the XDPProcessor.
+ * It can be mocked to do packet decoding testing.
+ * @author lucas
+ *
+ */
+public interface DatabaseConnection {
+	/**
+	 * Add a new order to the order-book specified by the symbolIndex
+	 * @param orderID 		unique orderID of the new order
+	 * @param symbolIndex 	symbolIndex of the stock, determining the order book
+	 * @param time_ns		timestamp from the sender in nanoseconds
+	 * @param symbolSeqNumber	symbol-sequence-number refers to an EPOCH time stamp
+	 * 			in seconds for this stock
+	 * @param price			price of the order
+	 * @param volume		volume of the order
+	 * @param isSell		true if it is sell/ask order and false if it is buy/bid
+	 * @param tradeSession	trade sessions for which the order is valid
+	 * @throws SQLException
+	 */
+	public void addOrder(long orderID, long symbolIndex, long time_ns, 
+			long symbolSeqNumber, long price, long volume, boolean isSell, 
+			int tradeSession) throws SQLException;
+	/**
+	 * modifies an existing active order-entry in the database by overwriting
+	 * its attributes price, volume, isSell and the time.
+	 * It has no effect to the database in case the order does not exists any more
+	 * in the active order book.   
+	 * @param orderID 		order ID of the order to be modified
+	 * @param symbolIndex 	defines the stock and the order-book in which 
+	 * 			the order resides
+	 * @param time_ns		timestamp from the sender in nanoseconds
+	 * @param symbolSeqNumber	symbol-sequence-number refers to an EPOCH time stamp
+	 * 			in seconds for this stock
+	 * @param price			potential new price of the order
+	 * @param volume		potential new volume of the order
+	 * @param isSell		true if it is sell/ask order and false if it is buy/bid
+	 * @throws SQLException
+	 */
+	public void modifyOrder(long orderID, long symbolIndex, long time_ns, 
+			long symbolSeqNumber, long price, long volume, boolean isSell) 
+					throws SQLException;
+	
+	public void reduceOrderVolume(long orderID, long symbolIndex, long time_ns,
+			long symbolSeqNumber, long volumeReduction);
+	
+	public void deleteOrder(long orderID, long symbolIndex, long time_ns, 
+			long symbolSeqNumber) throws SQLException;
+	
+	public void addTrade(long tradeID, long symbolIndex, long time_ns, 
+			long symbolSeqNumber, long prize, long volume) throws SQLException;
+	
+	public void addStockSummary(long symbolIndex, long time_s, long time_ns,
+			long lighPrice, long lowPrice, long openPrice, long closePrice,
+			long totalVolume) throws SQLException;
+	
+	public void correctTrade(long originalTradeID, long tradeID, long symbolIndex, 
+			long time_s, long time_ns, long symbolSeqNumber, long price,
+			long volume) throws SQLException;
+	/**
+	 * Adds a reference time to a stock specified by the symbol index,
+	 * the reference time maps to a symbol-sequence-number (other messages
+	 * refer to this sequence number to specify a time in seconds)
+	 * @param symbolIndex 		specifies the stock the time reference is connected to
+	 * @param symbolSeqNumber	sequence number of a sequence that is unique
+	 * 			for every symbol index
+	 * @param referenceTime		time in seconds (EPOCH) the sequence number maps to
+	 * @throws SQLException
+	 */
+	public void addSourceTimeReference(long symbolIndex, long symbolSeqNumber,
+			long referenceTime) throws SQLException;
+	
+	public void addSymbolMappingEntry(long symbolIndex, String symbol,
+			long priceScaleCode, long prevClosingPrice, long prevClosingVolume)
+				throws SQLException;
+	
+	//TODO: This is a misleading name, it should be removeTrade or addTradeCancellation
+	// how exactly are we implementing this in the database? -Lucas
+	public void cancelTrade(long tradeID, long symbolIndex, long time_s, 
+			long time_ns, long symbolSeqNumber) throws SQLException;
+	
+}
--- src/uk/ac/cam/cl/juliet/slave/xdpprocessing/XDPProcessorUnit.java
+++ src/uk/ac/cam/cl/juliet/slave/xdpprocessing/XDPProcessorUnit.java
@@ -1,24 +1,32 @@
 package uk.ac.cam.cl.juliet.slave.xdpprocessing;
 
+import java.sql.SQLException;
+
 import uk.ac.cam.cl.juliet.common.XDPRequest;
+import uk.ac.cam.cl.juliet.master.distribution.DatabaseConnectionUnit;
 import uk.ac.cam.cl.juliet.slave.xdpprocessing.Packets.Message;
 import uk.ac.cam.cl.juliet.slave.xdpprocessing.Packets.Packet;
 
 public class XDPProcessorUnit implements XDPProcessor {
+	
+	private DatabaseConnectionUnit mDB;
+	
+	public XDPProcessorUnit(DatabaseConnectionUnit pDBConnection) {
+		mDB = pDBConnection;
+	}
 
 	@Override
 	public boolean decode(XDPRequest packet) {
 		boolean result = true;
 		
 		Packet currentPacket = new Packet(packet.getPacketData());
-		// Check whether the packet is a new one (deliveryFlag 11)
-		// or a retransmission/error 
-		// Data processor will only emit packets with deliveryFlag 11 - Scott
 		
 		Message m = currentPacket.getNextMessage();
 		
 		while(m != null) {
 			switch( m.getMessageType()) {
+			case 2:
+				result &= decodeSourceTimeReferenceMessage(m);
 			case 3:
 				result &= decodeSymbolMappingMessage(m);
 				break;
@@ -37,8 +45,10 @@ public class XDPProcessorUnit implements XDPProcessor {
 			case 103:
 				result &= decodeOrderBookExecutionMessage(m);
 				break;
+			case 221:
+				result &= decodeTradeCancelOrBustMessage(m);
 			case 222:
-				result &= TradeCancelOrBustMessage(m);
+				result &= decodeTradeCorrectionMessage(m);
 				break;
 			case 223:
 				result &= StockSummaryMessage(m);
@@ -53,44 +63,190 @@ public class XDPProcessorUnit implements XDPProcessor {
 		return result;
 	}
 	
-	private boolean StockSummaryMessage(Message m) {
-		// TODO Auto-generated method stub
-		return false;
+	private boolean decodeSourceTimeReferenceMessage(Message m) {
+		long symbolIndex = m.readLong(4);
+		long symbolSequenceNumber = m.readLong(4);
+		long timeReference = m.readLong(4);
+		try {
+			mDB.addSourceTimeReference(symbolIndex, symbolSequenceNumber, timeReference);
+		} catch (SQLException e) {
+			// TODO write to log?
+			return false;
+		}
+		return true;
 	}
 
-	private boolean TradeCancelOrBustMessage(Message m) {
-		// TODO Auto-generated method stub
-		return false;
+	private boolean decodeTradeCorrectionMessage(Message m) {
+		
+		long sourceTime_s = m.readLong(4);
+		long sourceTime_ns = m.readLong(4);
+		long symbolIndex = m.readLong(4);
+		long symbolSequenceNumber = m.readLong(4);
+		long originalTradeID = m.readLong(4); 	//this is the name from the spec. A 
+												//better name would be orignialSequenceNumber 
+		long tradeID = m.readLong(4);
+		long price = m.readLong(4);
+		long volume = m.readLong(4);
+		// there are 5 more bytes in the message containing trade conditions
+		// but we are not interested in those
+		
+		try {
+			mDB.correctTrade(originalTradeID, tradeID, symbolIndex,
+					sourceTime_s, sourceTime_ns, symbolSequenceNumber,
+					price, volume);
+		} catch (SQLException e) {
+			// TODO write to log?
+			return false;
+		}
+		return true;
+	}
+
+	private boolean StockSummaryMessage(Message m) {
+		long sourceTime_s = m.readLong(4);
+		long sourceTime_ns = m.readLong(4);
+		long symbolIndex = m.readLong(4);
+		long highPrice = m.readLong(4);
+		long lowPrice = m.readLong(4);
+		long openPrice = m.readLong(4);
+		long closePrice = m.readLong(4);
+		long totalVolume = m.readLong(4);
+		
+		try {
+			mDB.addStockSummary(symbolIndex, sourceTime_s, sourceTime_ns,
+					highPrice, lowPrice, openPrice, closePrice, totalVolume);
+		} catch (SQLException e) {
+			// TODO write to error log
+			return false;
+		}
+		
+		//write to database
+		return true;
+	}
+
+	private boolean decodeTradeCancelOrBustMessage(Message m) {
+		long sourceTime_s = m.readLong(4);
+		long sourceTime_ns = m.readLong(4);
+		long symbolIndex = m.readLong(4);
+		long SymbolSequenceNumber = m.readLong(4);
+		long originalTradeID = m.readLong(4);
+		
+		try {
+			mDB.cancelTrade(originalTradeID, symbolIndex, sourceTime_s, sourceTime_ns, SymbolSequenceNumber);
+		} catch (SQLException e) {
+			// TODO write to error log or something similar?
+			return false;
+		}
+		return true;
 	}
 
 	private boolean decodeOrderBookExecutionMessage(Message m) {
-		// TODO Auto-generated method stub
-		return false;
+		long sourcetime_ns = m.readLong(4);
+		long symbolIndex = m.readLong(4);
+		long symbolSequenceNumber = m.readLong(4);
+		long orderID = m.readLong(4);
+		long price = m.readLong(4);
+		long volume = m.readLong(4);
+		boolean isGTC = (m.readUnsignedByte()== 1);
+		int reasonCode = m.readUnsignedByte();
+		long tradeID = m.readLong(4);
+		
+		try {
+			mDB.addTrade(tradeID, symbolIndex, sourcetime_ns, 
+					symbolSequenceNumber, price, volume);
+			// if the reasonCode is not zero, no extra modify or delete message 
+			//will be sent for that order:
+			if (reasonCode == 7) {
+				//an order is partially filled
+				mDB.reduceOrderVolume(orderID, symbolIndex, sourcetime_ns,
+						symbolSequenceNumber, volume);
+			} else if (reasonCode == 3) {
+				//an order is fully executed
+				mDB.deleteOrder(orderID, symbolIndex, sourcetime_ns,
+						symbolSequenceNumber);
+			} else {
+				//the reasonCode is invalid
+				//TODO throw an exception or have an error log!
+			}
+			
+		} catch (SQLException e) {
+			// TODO write to error log?
+			return false;
+		}
+		return true;
 	}
 
 	private boolean decodeOrderBookDeleteOrderMessage(Message m) {
-		// TODO Auto-generated method stub
-		return false;
+		long sourceTime_ns = m.readLong(4);
+		long symbolIndex = m.readLong(4);
+		long symbolSequenceNumber = m.readLong(4);
+		long orderID = m.readLong(4);
+		boolean isSell = m.readChar()== 'B';
+		// there are two more bytes containing OrderIDGTCIndicator and a ReasonCode
+		// but we don't need them now
+
+		try {
+			mDB.deleteOrder(orderID, symbolIndex, sourceTime_ns, symbolSequenceNumber);
+		} catch (SQLException sqle) {
+			//TODO print to an error log?
+			return false;
+		}
+		return true;
 	}
 
 	private boolean decodeOrderBookModifyOrderMessage(Message m) {
-		// TODO Auto-generated method stub
-		return false;
+		long sourceTime_ns = m.readLong(4);
+		long symbolIndex = m.readLong(4);
+		long SymbolSequenceNumber = m.readLong(4);
+		long orderID = m.readLong(4);
+		long price = m.readLong(4);
+		long volume = m.readLong(4);
+		boolean isSell = m.readChar()== 'B';
+		// there are two more bytes containing OrderIDGTCIndicator and a ReasonCode
+		
+		try {
+			mDB.modifyOrder(orderID, symbolIndex, sourceTime_ns,
+					SymbolSequenceNumber, price, volume, isSell);
+		} catch (SQLException e) {
+			// TODO write to an error log?
+			return false;
+		}
+		return true;
 	}
 
 	private boolean decodeOrderBookAddOrderMessage(Message m) {
-		// TODO Auto-generated method stub
-		return false;
+		long sourceTime_ns = m.readLong(4);
+		long symbolIndex = m.readLong(4);
+		long symbolSequenceNumber = m.readLong(4);
+		long orderID = m.readLong(4);
+		long price = m.readLong(4);
+		long volume = m.readLong(4);
+		boolean isSell = m.readChar()== 'S';
+		boolean isGTC = (m.readUnsignedByte()== 1);
+		int tradeSession = m.readUnsignedByte();
+		
+		try {
+			mDB.addOrder(orderID, symbolIndex, sourceTime_ns, 
+					symbolSequenceNumber, price, volume, isSell, tradeSession);
+		} catch (SQLException e) {
+			// TODO print to error log?
+			return false;
+		}
+		return true;
 	}
 
 	private boolean decodeTradeSessionChangeMessage(Message m) {
-		// TODO Auto-generated method stub
+		long sourceTime_s = m.readLong(4);
+		long sourceTime_ns = m.readLong(4);
+		long symbolIndex = m.readLong(4);
+		long symbolSequenceNumber = m.readLong(4);
+		int tradingSession = m.readUnsignedByte();
+		// TODO update database
 		return false;
 	}
 
 	private boolean decodeSymbolMappingMessage(Message m) {
 		
-		int symbolIndex = (int) m.readLong(4);
+		long symbolIndex = m.readLong(4);
 		String symbol = m.readString(11); 
 		// Jump the filler
 		m.readChar();
@@ -107,8 +263,13 @@ public class XDPProcessorUnit implements XDPProcessor {
 		long priceResolution = m.readLong(1);
 		long roundLot = m.readChar();
 		
-		//TODO write to the database
-		
+		try {
+			mDB.addSymbolMappingEntry(symbolIndex, symbol, priceScaleCode, 
+					prevClosePrice, prevCloseVolume);
+		} catch (SQLException e) {
+			// TODO write to error-log or something?
+			return false;
+		}
 		return true;
 	}
 
