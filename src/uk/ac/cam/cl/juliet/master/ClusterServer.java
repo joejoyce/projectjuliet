a4acc1502eb76b3a59d60cdb2ba51a401a52c732
joejoyce <josephjoyce@gmx.com>
2014-02-04 15:53:41 +0000

    Fixed errors to do with moving ClusterServer - should've probably done this the first time!

---
+++ src/uk/ac/cam/cl/juliet/master/ClusterServer.java
@@ -0,0 +1,29 @@
+package uk.ac.cam.cl.juliet.master.clustermanagement.distribution;
+
+import java.io.IOException;
+
+import uk.ac.cam.cl.juliet.common.StringTestPacket;
+import uk.ac.cam.cl.juliet.master.clustermanagement.distribution.ClusterMaster;
+import uk.ac.cam.cl.juliet.master.clustermanagement.distribution.NoClusterException;
+
+/**
+ * Prepare the ClusterServer for operation - also run some tests?
+ * @author joseph
+ *
+ */
+public class ClusterServer {
+	
+	public static void main(String args[]) throws IOException {
+		ClusterMaster m = new ClusterMaster();
+		m.start(5000);
+		try {
+			m.sendPacket(new StringTestPacket("Is this working?"));
+		} catch (NoClusterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//Run all tests: - include delay to bring Raspberry Pis up?
+		System.in.read();
+		m.stop();
+	}
+}
