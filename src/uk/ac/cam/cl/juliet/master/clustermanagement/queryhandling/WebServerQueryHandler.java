e96f0bbf7b51ed7ccbd1a46a890d1307487e4647
slwilliams <slw69@cam.ac.uk>
2014-02-04 22:19:40 +0000

    Fixed json conversion bugs

--- src/uk/ac/cam/cl/juliet/master/clustermanagement/queryhandling/WebServerQueryHandler.java
+++ src/uk/ac/cam/cl/juliet/master/clustermanagement/queryhandling/WebServerQueryHandler.java
@@ -11,6 +11,7 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import uk.ac.cam.cl.juliet.common.QueryPacket;
 /**
@@ -35,8 +36,8 @@ public class WebServerQueryHandler implements QueryHandler, Runnable {
 			BufferedReader din  = new BufferedReader(new InputStreamReader(new BufferedInputStream(server.getInputStream())));
 			PrintWriter pw = new PrintWriter(server.getOutputStream(), true);
 			
-			String query = din.readLine();
-			String[] splitQuery = query.split("|");
+			String query = din.readLine();			
+			String[] splitQuery = query.split("\\|");
 			
 			
 			switch(splitQuery[0]) {
@@ -74,37 +75,38 @@ public class WebServerQueryHandler implements QueryHandler, Runnable {
 	private String toJSON(ResultSet r) throws SQLException {
 		ResultSetMetaData rsmd = r.getMetaData();
 		int columnCount = rsmd.getColumnCount();
-		int rowCount = r.getRow();
 		String[] columnNames = new String[columnCount];
 		
 		for(int i = 0; i < columnCount; i ++) {
 			columnNames[i] = rsmd.getColumnName(i);
 		}
 		
-		String[][] results = new String[rowCount][columnCount];
+		//String[][] results = new String[rowCount][columnCount];
+		ArrayList<ArrayList<String>> results = new ArrayList<ArrayList<String>>();
 		
-		int row = 0;
 		
 		while(r.next()) {
-			for(int i = 0; i < columnCount; i ++) {
-				results[row++][i] = r.getString(i);
-			}			
+			ArrayList<String> a = new ArrayList<String>();
+			for(int i = 1; i <= columnCount; i ++) {
+				a.add(r.getString(i));
+			}
+			results.add(a);
 		}		
 		
 		String result = "";
 		
-		for(int i = 0; i < results.length; i ++) {
-			String rowJSON = singleRowToJSON(results[i], columnNames);
+		for(int i = 0; i < results.size(); i ++) {
+			String rowJSON = singleRowToJSON(results.get(i), columnNames);
 			result += rowJSON + ",";
 		}
 		
 		return "[" + result.substring(0, result.length() - 1) + "]";
 	}
 	
-	private String singleRowToJSON(String[] row, String[] columnNames) {
+	private String singleRowToJSON(ArrayList<String> row, String[] columnNames) {
 		String result = "";
-		for(int i = 0; i < row.length; i ++) {
-			result += columnNames[i] + ": " + "\"" + row[i] + "\"" + ",";
+		for(int i = 0; i < row.size(); i ++) {
+			result += columnNames[i] + ": " + "\"" + row.get(i) + "\"" + ",";
 		}
 		
 		return "{" + result.substring(0, result.length() - 1) + "}";
