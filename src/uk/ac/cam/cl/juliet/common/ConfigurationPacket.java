2e9387d5a649ffc8184b41d68182e41336d4c9fc
joejoyce <josephjoyce@gmx.com>
2014-02-04 16:11:58 +0000

    Made ConfigurationPacket work

--- src/uk/ac/cam/cl/juliet/common/ConfigurationPacket.java
+++ src/uk/ac/cam/cl/juliet/common/ConfigurationPacket.java
@@ -1,5 +1,13 @@
 package uk.ac.cam.cl.juliet.common;
 
+import java.util.HashMap;
+
 public class ConfigurationPacket extends Container {
-	
+	HashMap <String,String>h = new HashMap<String,String>();
+	public String getSetting(String name) {
+		return h.get(name);
+	}
+	public void setSetting(String name, Object ob) {
+		h.put(name,ob.toString());
+	}
 }
