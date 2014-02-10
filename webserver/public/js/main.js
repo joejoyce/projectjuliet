f5fe079c3dac9619cadb5beaad231764592e7d62
slwilliams <slw69@cam.ac.uk>
2014-02-10 20:35:38 +0000

    removed alerts

--- webserver/public/js/main.js
+++ webserver/public/js/main.js
@@ -1,13 +1,6 @@
 $(document).ready(function() {
- 	console.log("test");
- 	alert("gay");
-	var myData = new Array([10, 20], [15, 10], [20, 30], [25, 10], [30, 5]);
-	alert("gay1");
+ 	var myData = new Array([10, 20], [15, 10], [20, 30], [25, 10], [30, 5]);
 	var myChart = new JSChart('chartcontainer', 'line');
-	alert("gay2");
 	myChart.setDataArray(myData);
-	alert("gay3");
-	
-	myChart.draw();
-	
+	myChart.draw();	
 });
