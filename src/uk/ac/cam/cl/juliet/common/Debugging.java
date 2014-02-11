1411e19735635e1cacead70e1e7e5dca27572c7b
joejoyce <josephjoyce@gmx.com>
2014-02-11 15:14:06 +0000

    Added priority to debug

--- src/uk/ac/cam/cl/juliet/common/Debugging.java
+++ src/uk/ac/cam/cl/juliet/common/Debugging.java
@@ -21,14 +21,14 @@ class OutputWrap {
 	public OutputWrap(Queue<Container>q ){
 		this.q = q;
 	}
-	public void send ( String str) {
+	public void send ( String str, int pri) {
 		if(printout != null)
 			printout.print(str); //Hopefully this doesn't need to be thread safe
 		else if (null != q) {
 			q.add( new DebugMsg(str));
 		} else {
 			try {
-				ojout.writeObject( new DebugMsg(str));
+				ojout.writeObject( new DebugMsg(str,pri));
 			} catch (IOException e) {
 				System.err.println("Error sending error to host!");
 			}
@@ -38,7 +38,8 @@ class OutputWrap {
 
 public class Debugging {
 	private static String myAddr = null;
-	private static boolean debug = false;
+	private static int priority = 0;
+	private static int default_priority = 5;
 	
 	private static LinkedList<OutputWrap> out = new LinkedList<OutputWrap>();
 	
@@ -49,12 +50,11 @@ public class Debugging {
 		myAddr = addr;
 	}
 	
-	public static void enableDebugging() {
-		debug = true;
+	public static void setPriority(int pri) {
+		priority = pri;
 	}
-	
-	public static void disableDebugging() {
-		debug = false;
+	public static int getPriority() {
+		return priority;
 	}
 
 	public static void registerOutputLocation( PrintStream ps ) {
@@ -74,23 +74,33 @@ public class Debugging {
 		out.add(new OutputWrap(q));
 	}
 	
-	private static void send(String str) {
+	private static void send(String str, int pri) {
 		for(OutputWrap o : out) {
-			o.send(str);
+			o.send(str,pri);
 		}
 	}
 	
 	public static void println(String str) {
-		if(debug)
-			send(str + "\n");
+		if(priority >= default_priority)
+			send(str + "\n",default_priority);
 	}
 	
 	public static void print(String str) {
-		if(debug)
-			send(str);
+		if(priority >= default_priority)
+			send(str,default_priority);
+	}
+	public static void println(int pri, String str) {
+		if(priority >= pri)
+			send(str + "\n",pri);
+	}
+	
+	public static void print(int pri, String str) {
+		if(priority >= pri)
+			send(str,pri);
 	}
 	
 	public static void recieveDebug( DebugMsg msg ) {
-		send(msg.toString()); //TODO also add information about location? 
+		if(priority >= msg.getPriority())
+			send(msg.toString(),msg.getPriority()); //TODO also add information about location? 
 	}
 }
