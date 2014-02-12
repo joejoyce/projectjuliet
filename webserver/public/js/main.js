263f23da6165cc6c9c8cab2a530b564739a06b16
slwilliams <slw69@cam.ac.uk>
2014-02-12 23:05:47 +0000

    Added cool stock price graphs

--- webserver/public/js/main.js
+++ webserver/public/js/main.js
@@ -1,6 +1,29 @@
 $(document).ready(function() {
- 	var myData = new Array([10, 20], [15, 10], [20, 30], [25, 10], [30, 5]);
-	var myChart = new JSChart('chartcontainer', 'line');
-	myChart.setDataArray(myData);
-	myChart.draw();	
+ 	var stockData = $('meta[name=stock-data]').attr("content");
+ 	stockData = $.parseJSON(stockData);
+ 	var priceScale = Number($('meta[name=price-scale]').attr("content"));
+ 	var symbol = $('meta[name=symbol]').attr("content");
+ 	var chartData = [];
+ 	var x = 0;
+ 	$(stockData).each(function(i, data) {
+ 		chartData.push(["Volume: " + data.volume,data.price*priceScale]);
+ 		x += 1;
+ 	});
+ 	$('#chartcontainer').highcharts('StockChart', {
+			rangeSelector : {
+				selected : 1
+			},
+
+			title : {
+				text : symbol + ' Stock Price'
+			},
+			
+			series : [{
+				name : symbol,
+				data : chartData,
+				tooltip: {
+					valueDecimals: 2
+				}
+			}]
+	});
 });
