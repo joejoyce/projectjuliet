858b210d81ae96dd282a01e92cb3324abe3e7ae2
Scott Williams <slw69@cam.ac.uk>
2014-02-14 20:56:32 +0000

    hope this doesnt break it

--- webserver/controllers/status.js
+++ webserver/controllers/status.js
@@ -1,10 +1,25 @@
+var net = require('net');
+
 /**
  * GET /status
  * System status page
  */
 
 exports.getStatus = function(req, res) {
-  res.render('status', {
-    title: 'Status'
+  var client = net.connect(1337, 'localhost');
+  client.setEncoding('utf8');
+
+  console.log("hello");
+
+  client.write('status|listclients\n');
+
+  client.on('data', function(data) {
+    var dataObj = JSON.parse(data);
+console.log("data: " + dataObj);
+    res.render('status', {
+      title: 'Status',
+      data: dataObj
+    });
+    client.end();
   });
 };
