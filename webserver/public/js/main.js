65b4fd47fcf46f7febbb3222084e111a95061dc6
Michael Hsu <mpahsu@gmail.com>
2014-02-20 15:45:30 +0000

    Missed out parameter list.

--- webserver/public/js/main.js
+++ webserver/public/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('#filter').fastLiveFilter('#symbols');
-	$('#filter-form').on('submit', function{
+	$('#filter-form').on('submit', function() {
 		var visibleSymbols = $('#symbols li:visible');
 		if (visibleSymbols.length == 1) {
 			window.location.href = "/order-book/" + visibleSymbols.data('id');
