ec63b50279fb30abd393e1bba0983d868fd67eab
Michael Hsu <mpahsu@gmail.com>
2014-02-20 15:42:33 +0000

    Redirect user to symbol's page if only one symbol in menu.

--- webserver/public/js/main.js
+++ webserver/public/js/main.js
@@ -1,3 +1,9 @@
-$(function() {
+$(document).ready(function() {
 	$('#filter').fastLiveFilter('#symbols');
+	$('#filter-form').on('submit', function{
+		var visibleSymbols = $('#symbols li:visible');
+		if (visibleSymbols.length == 1) {
+			window.location.href = "/order-book/" + visibleSymbols.data('id');
+		}
+	});
 });
