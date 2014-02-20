fe6771580b7d752b1478c9624f52d082e9c84e22
Michael Hsu <mpahsu@gmail.com>
2014-02-20 16:23:53 +0000

    Prevent default behaviour of form submit event.

--- webserver/public/js/main.js
+++ webserver/public/js/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
 	$('#filter').fastLiveFilter('#symbols');
-	$('#filter-form').on('submit', function() {
+	$('#filter-form').on('submit', function(e) {
 		var visibleSymbols = $('#symbols li:visible');
 		if (visibleSymbols.length == 1) {
 			window.location.href = visibleSymbols.find('a').attr('href');
 		}
+		e.preventDefault();
 	});
 });
