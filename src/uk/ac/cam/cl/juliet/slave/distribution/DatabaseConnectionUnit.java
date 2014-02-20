bcf1702a8643b770be063a289759ce1f1da209b5
Dylan McDermott <dm606@cam.ac.uk>
2014-02-20 18:50:37 +0000

    Reimplemented add order query

--- src/uk/ac/cam/cl/juliet/slave/distribution/DatabaseConnectionUnit.java
+++ src/uk/ac/cam/cl/juliet/slave/distribution/DatabaseConnectionUnit.java
@@ -20,7 +20,7 @@ public class DatabaseConnectionUnit implements DatabaseConnection {
 
 	public DatabaseConnectionUnit(Connection c) throws SQLException {
 		this.connection = c;
-		this.addOrderBatch = connection.prepareStatement("INSERT INTO order_book VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, 1, 0)");
+		this.addOrderBatch = connection.prepareStatement("CALL addOrder(?, ?, ?, ?, ?, ?, ?, ?, ?)");
 		this.addTradeBatch = connection.prepareStatement("INSERT INTO trade VALUES (?, ?, ?, ?, ?, ?)");
 		// this.deleteOrderBatch = connection.prepareStatement("UPDATE order_book SET is_deleted=1 WHERE (order_id = ?) AND (symbol_id = ?)");
 		this.deleteOrderBatch = connection.prepareStatement("DELETE FROM order_book WHERE (order_id = ?) AND (symbol_id = ?)");
